1545e7ee970cd68b45b306149b70afb2da34ce3f
danielfernandoaraujo <danielferna11@gmail.com>
2022-05-06 17:21:48 -0300

    Alteração na pagina inicial

--- src/pages/home/styled.js
+++ src/pages/home/styled.js
@@ -6,9 +6,7 @@ export const HomePage = styled.div`
     margin: 0 ;
     height: 100vh ;
     background-color: black ;
-    background: #2193b0;  /* fallback for old browsers */
-    background: -webkit-linear-gradient(to right, #6dd5ed, #2193b0);  /* Chrome 10-25, Safari 5.1-6 */
-    background: linear-gradient(to right, #6dd5ed, #2193b0); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    background: #17b0ec;
         .aviso{
             position: absolute;
             display: flex ;
